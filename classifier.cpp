21dae74c813c8f1f35d9257bdbcf273068a2595e
Keegan Stewart <kestewart@OMC-Kumpf.local>
2021-04-23 23:17:45 -0400

    Added Classifier file, class and data member

---
+++ classifier.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // This header Provides basic input and output services for C++ programs
+using namespace std; // This will allow you to use the string class without using  std:: (I will explain more about the namespace next lecture.)
+
+class Classifier {
+
+private:
+	int data;
+
+
+};
+
+
